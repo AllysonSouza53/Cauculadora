8f6bff351e1ee38cb56e1f0047508d49619e1d2a
Usuario <Usuario@CPD02-M18>
2024-05-14 12:19:30 -0300

    Adicionar arquivos de projeto.

---
+++ Cauculadora/Form1.Designer.cs
@@ -0,0 +1,309 @@
+﻿namespace Cauculadora
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.Num_8 = new System.Windows.Forms.Button();
+            this.Num_9 = new System.Windows.Forms.Button();
+            this.Multiplicar = new System.Windows.Forms.Button();
+            this.Num_4 = new System.Windows.Forms.Button();
+            this.Num_5 = new System.Windows.Forms.Button();
+            this.Num_6 = new System.Windows.Forms.Button();
+            this.Menos = new System.Windows.Forms.Button();
+            this.Num_3 = new System.Windows.Forms.Button();
+            this.Num_1 = new System.Windows.Forms.Button();
+            this.Num_2 = new System.Windows.Forms.Button();
+            this.Mais = new System.Windows.Forms.Button();
+            this.MaisMenos = new System.Windows.Forms.Button();
+            this.P100 = new System.Windows.Forms.Button();
+            this.Dividir = new System.Windows.Forms.Button();
+            this.Zerar = new System.Windows.Forms.Button();
+            this.Num_7 = new System.Windows.Forms.Button();
+            this.Num_0 = new System.Windows.Forms.Button();
+            this.virgula = new System.Windows.Forms.Button();
+            this.Igual = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(227, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Num_8
+            // 
+            this.Num_8.Location = new System.Drawing.Point(145, 226);
+            this.Num_8.Name = "Num_8";
+            this.Num_8.Size = new System.Drawing.Size(90, 80);
+            this.Num_8.TabIndex = 7;
+            this.Num_8.Text = "8";
+            this.Num_8.UseVisualStyleBackColor = true;
+            this.Num_8.Click += new System.EventHandler(this.Num_8_Click_1);
+            // 
+            // Num_9
+            // 
+            this.Num_9.Location = new System.Drawing.Point(241, 226);
+            this.Num_9.Name = "Num_9";
+            this.Num_9.Size = new System.Drawing.Size(90, 80);
+            this.Num_9.TabIndex = 8;
+            this.Num_9.Text = "9";
+            this.Num_9.UseVisualStyleBackColor = true;
+            this.Num_9.Click += new System.EventHandler(this.Num_9_Click);
+            // 
+            // Multiplicar
+            // 
+            this.Multiplicar.Location = new System.Drawing.Point(337, 226);
+            this.Multiplicar.Name = "Multiplicar";
+            this.Multiplicar.Size = new System.Drawing.Size(90, 80);
+            this.Multiplicar.TabIndex = 9;
+            this.Multiplicar.Text = "x";
+            this.Multiplicar.UseVisualStyleBackColor = true;
+            this.Multiplicar.Click += new System.EventHandler(this.Multiplicar_Click);
+            // 
+            // Num_4
+            // 
+            this.Num_4.Location = new System.Drawing.Point(49, 312);
+            this.Num_4.Name = "Num_4";
+            this.Num_4.Size = new System.Drawing.Size(90, 80);
+            this.Num_4.TabIndex = 10;
+            this.Num_4.Text = "4";
+            this.Num_4.UseVisualStyleBackColor = true;
+            this.Num_4.Click += new System.EventHandler(this.Num_4_Click);
+            // 
+            // Num_5
+            // 
+            this.Num_5.Location = new System.Drawing.Point(145, 312);
+            this.Num_5.Name = "Num_5";
+            this.Num_5.Size = new System.Drawing.Size(90, 80);
+            this.Num_5.TabIndex = 11;
+            this.Num_5.Text = "5";
+            this.Num_5.UseVisualStyleBackColor = true;
+            this.Num_5.Click += new System.EventHandler(this.Num_5_Click);
+            // 
+            // Num_6
+            // 
+            this.Num_6.Location = new System.Drawing.Point(241, 312);
+            this.Num_6.Name = "Num_6";
+            this.Num_6.Size = new System.Drawing.Size(90, 80);
+            this.Num_6.TabIndex = 12;
+            this.Num_6.Text = "6";
+            this.Num_6.UseVisualStyleBackColor = true;
+            this.Num_6.Click += new System.EventHandler(this.Num_6_Click);
+            // 
+            // Menos
+            // 
+            this.Menos.Location = new System.Drawing.Point(337, 312);
+            this.Menos.Name = "Menos";
+            this.Menos.Size = new System.Drawing.Size(90, 80);
+            this.Menos.TabIndex = 13;
+            this.Menos.Text = "-";
+            this.Menos.UseVisualStyleBackColor = true;
+            this.Menos.Click += new System.EventHandler(this.Menos_Click);
+            // 
+            // Num_3
+            // 
+            this.Num_3.Location = new System.Drawing.Point(241, 398);
+            this.Num_3.Name = "Num_3";
+            this.Num_3.Size = new System.Drawing.Size(90, 80);
+            this.Num_3.TabIndex = 14;
+            this.Num_3.Text = "3";
+            this.Num_3.UseVisualStyleBackColor = true;
+            this.Num_3.Click += new System.EventHandler(this.Num_3_Click);
+            // 
+            // Num_1
+            // 
+            this.Num_1.Location = new System.Drawing.Point(49, 398);
+            this.Num_1.Name = "Num_1";
+            this.Num_1.Size = new System.Drawing.Size(90, 80);
+            this.Num_1.TabIndex = 15;
+            this.Num_1.Text = "1";
+            this.Num_1.UseVisualStyleBackColor = true;
+            this.Num_1.Click += new System.EventHandler(this.Num_1_Click);
+            // 
+            // Num_2
+            // 
+            this.Num_2.Location = new System.Drawing.Point(145, 398);
+            this.Num_2.Name = "Num_2";
+            this.Num_2.Size = new System.Drawing.Size(90, 80);
+            this.Num_2.TabIndex = 16;
+            this.Num_2.Text = "2";
+            this.Num_2.UseVisualStyleBackColor = true;
+            this.Num_2.Click += new System.EventHandler(this.Num_2_Click);
+            // 
+            // Mais
+            // 
+            this.Mais.Location = new System.Drawing.Point(337, 398);
+            this.Mais.Name = "Mais";
+            this.Mais.Size = new System.Drawing.Size(90, 80);
+            this.Mais.TabIndex = 17;
+            this.Mais.Text = "+";
+            this.Mais.UseVisualStyleBackColor = true;
+            this.Mais.Click += new System.EventHandler(this.Mais_Click);
+            // 
+            // MaisMenos
+            // 
+            this.MaisMenos.Location = new System.Drawing.Point(145, 140);
+            this.MaisMenos.Name = "MaisMenos";
+            this.MaisMenos.Size = new System.Drawing.Size(90, 80);
+            this.MaisMenos.TabIndex = 18;
+            this.MaisMenos.Text = "+/-";
+            this.MaisMenos.UseVisualStyleBackColor = true;
+            this.MaisMenos.Click += new System.EventHandler(this.MaisMenos_Click);
+            // 
+            // P100
+            // 
+            this.P100.Location = new System.Drawing.Point(241, 140);
+            this.P100.Name = "P100";
+            this.P100.Size = new System.Drawing.Size(90, 80);
+            this.P100.TabIndex = 20;
+            this.P100.Text = "%";
+            this.P100.UseVisualStyleBackColor = true;
+            this.P100.Click += new System.EventHandler(this.P100_Click);
+            // 
+            // Dividir
+            // 
+            this.Dividir.Location = new System.Drawing.Point(337, 140);
+            this.Dividir.Name = "Dividir";
+            this.Dividir.Size = new System.Drawing.Size(90, 80);
+            this.Dividir.TabIndex = 21;
+            this.Dividir.Text = "/";
+            this.Dividir.UseVisualStyleBackColor = true;
+            this.Dividir.Click += new System.EventHandler(this.Dividir_Click);
+            // 
+            // Zerar
+            // 
+            this.Zerar.Location = new System.Drawing.Point(49, 140);
+            this.Zerar.Name = "Zerar";
+            this.Zerar.Size = new System.Drawing.Size(90, 80);
+            this.Zerar.TabIndex = 22;
+            this.Zerar.Text = "C";
+            this.Zerar.UseVisualStyleBackColor = true;
+            this.Zerar.Click += new System.EventHandler(this.Zerar_Click);
+            // 
+            // Num_7
+            // 
+            this.Num_7.Location = new System.Drawing.Point(49, 226);
+            this.Num_7.Name = "Num_7";
+            this.Num_7.Size = new System.Drawing.Size(90, 80);
+            this.Num_7.TabIndex = 23;
+            this.Num_7.Text = "7";
+            this.Num_7.UseVisualStyleBackColor = true;
+            this.Num_7.Click += new System.EventHandler(this.Num_7_Click);
+            // 
+            // Num_0
+            // 
+            this.Num_0.Location = new System.Drawing.Point(49, 484);
+            this.Num_0.Name = "Num_0";
+            this.Num_0.Size = new System.Drawing.Size(90, 80);
+            this.Num_0.TabIndex = 24;
+            this.Num_0.Text = "0";
+            this.Num_0.UseVisualStyleBackColor = true;
+            this.Num_0.Click += new System.EventHandler(this.Num_0_Click);
+            // 
+            // virgula
+            // 
+            this.virgula.Location = new System.Drawing.Point(145, 484);
+            this.virgula.Name = "virgula";
+            this.virgula.Size = new System.Drawing.Size(90, 80);
+            this.virgula.TabIndex = 25;
+            this.virgula.Text = ",";
+            this.virgula.UseVisualStyleBackColor = true;
+            this.virgula.Click += new System.EventHandler(this.virgula_Click);
+            // 
+            // Igual
+            // 
+            this.Igual.Location = new System.Drawing.Point(241, 484);
+            this.Igual.Name = "Igual";
+            this.Igual.Size = new System.Drawing.Size(186, 80);
+            this.Igual.TabIndex = 27;
+            this.Igual.Text = "=";
+            this.Igual.UseVisualStyleBackColor = true;
+            this.Igual.Click += new System.EventHandler(this.Igual_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 601);
+            this.Controls.Add(this.Igual);
+            this.Controls.Add(this.virgula);
+            this.Controls.Add(this.Num_0);
+            this.Controls.Add(this.Num_7);
+            this.Controls.Add(this.Zerar);
+            this.Controls.Add(this.Dividir);
+            this.Controls.Add(this.P100);
+            this.Controls.Add(this.MaisMenos);
+            this.Controls.Add(this.Mais);
+            this.Controls.Add(this.Num_2);
+            this.Controls.Add(this.Num_1);
+            this.Controls.Add(this.Num_3);
+            this.Controls.Add(this.Menos);
+            this.Controls.Add(this.Num_6);
+            this.Controls.Add(this.Num_5);
+            this.Controls.Add(this.Num_4);
+            this.Controls.Add(this.Multiplicar);
+            this.Controls.Add(this.Num_9);
+            this.Controls.Add(this.Num_8);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Num_8;
+        private System.Windows.Forms.Button Num_9;
+        private System.Windows.Forms.Button Multiplicar;
+        private System.Windows.Forms.Button Num_4;
+        private System.Windows.Forms.Button Num_5;
+        private System.Windows.Forms.Button Num_6;
+        private System.Windows.Forms.Button Menos;
+        private System.Windows.Forms.Button Num_3;
+        private System.Windows.Forms.Button Num_1;
+        private System.Windows.Forms.Button Num_2;
+        private System.Windows.Forms.Button Mais;
+        private System.Windows.Forms.Button MaisMenos;
+        private System.Windows.Forms.Button P100;
+        private System.Windows.Forms.Button Dividir;
+        private System.Windows.Forms.Button Zerar;
+        private System.Windows.Forms.Button Num_7;
+        private System.Windows.Forms.Button Num_0;
+        private System.Windows.Forms.Button virgula;
+        private System.Windows.Forms.Button Igual;
+    }
+}
+
---
+++ Cauculadora/Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cauculadora
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void MaisMenos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Zerar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_8_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_8_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void P100_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Dividir_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Multiplicar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Menos_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Mais_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Num_0_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void virgula_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Igual_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
